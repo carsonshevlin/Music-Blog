2a2d2c93cd3a19bb1855ac00cbe6e9492ff61915
Carson Shevlin <carsonshevlin@outlook.com>
2023-05-29 18:37:49 -0400

    Adding home, editor, and blog page

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyBkJPOdyubcAw-0l2zskIJZ1lwa9HksPZ4",
+  authDomain: "blogging-website-f862b.firebaseapp.com",
+  projectId: "blogging-website-f862b",
+  storageBucket: "blogging-website-f862b.appspot.com",
+  messagingSenderId: "918752956540",
+  appId: "1:918752956540:web:9f3fa621bdfbec8f6e79f9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
